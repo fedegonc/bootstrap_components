11310925f8092e269ea72bec36dd596d735a5bb2
federico goncalvez <97002761+fedegonc@users.noreply.github.com>
2024-11-20 14:30:35 -0300

    Update router.js

--- router.js
+++ router.js
@@ -3,12 +3,12 @@ class Router {
     navigate(page) {
         console.log("Navigating to:", page);
         const url = new URL(window.location);
-        url.hash = page;
+        url.pathname = `/${page}`;
         window.history.pushState({}, '', url);
         
         document.getElementById('content').innerHTML = pages[page].render();
+        console.log(`Estás en la ruta: ${url.pathname}`);
     }
 }
 
 const router = new Router();
-
